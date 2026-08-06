c6de1a4b22431c9c465463f8f139a0cd7538bace
Carme <ballecarmela@gmail.com>
2026-08-06 12:36:03 -0300

    create class loginPageData

---
+++ pages/login-pages/login-page.data.ts
@@ -0,0 +1,16 @@
+export class LoginPageData{
+    static get credentials(){
+        return{
+            usernames:{
+                standardUser: 'standard_user',
+                lockedOutUser: 'locked_out_user',
+                problemUser: 'problem_user',
+                performanceGlitchUser: 'performance_glitch_user',
+                errorUser: 'error_user',
+                visualUser: 'visual_user'
+            },
+            password: 'secret_sauce'
+            
+        }
+    }
+}
--- tests/test01.spec.ts
+++ tests/test01.spec.ts
@@ -1,15 +1,19 @@
 import {test} from '@playwright/test' 
 import { CommonPageMethods } from '../pages/common-page/common-page.methods'
 import { LoginPageMethods } from '../pages/login-pages/login-pages.methods'
+import { LoginPageData } from '../pages/login-pages/login-page.data'
+
+const userCredentials = LoginPageData.credentials;
 
 test('Login', async ({page}) => {
     
     const commonPageMethods = new CommonPageMethods(page)
-    const loginPageElements = new LoginPageMethods(page)
+    const loginPageMethods = new LoginPageMethods(page)
 
     await commonPageMethods.navigateToTheApplication()
-    await loginPageElements.insertUsername('standard_user')
-    await loginPageElements.insertPassword('secret_sauce')
+    await loginPageMethods.insertUsername(userCredentials.usernames.standardUser)
+    await loginPageMethods.insertPassword(userCredentials.password)
+    await loginPageMethods.clickOnLoginButton()
     await page.waitForTimeout(4000)
 
 })
