bc50964eda5f195b21bcc819b83a17592ef2a610
Carme <ballecarmela@gmail.com>
2026-08-06 11:32:52 -0300

    Create login class methods

--- tests/test01.spec.ts
+++ tests/test01.spec.ts
@@ -1,7 +1,15 @@
 import {test} from '@playwright/test' 
 import { CommonPageMethods } from '../pages/common-page/common-page.methods'
+import { LoginPageMethods } from '../pages/login-pages/login-pages.methods'
 
 test('Login', async ({page}) => {
+    
     const commonPageMethods = new CommonPageMethods(page)
+    const loginPageElements = new LoginPageMethods(page)
+
     await commonPageMethods.navigateToTheApplication()
+    await loginPageElements.insertUsername('standard_user')
+    await loginPageElements.insertPassword('secret_sauce')
+    await page.waitForTimeout(4000)
+
 })
